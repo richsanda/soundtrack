9fcba5ce627fc08fd6d5617bff52fdc54827c9d3
rich <richsanda@yahoo.com>
2016-12-28 11:55:20 -0500

    make extracting hash tags and name tags use the same regex

--- src/main/java/w/whateva/soundtrack/service/util/SoundtrackUtil.java
+++ src/main/java/w/whateva/soundtrack/service/util/SoundtrackUtil.java
@@ -11,27 +11,30 @@ import java.util.regex.Pattern;
  */
 public class SoundtrackUtil {
 
-    private final static Pattern namePattern = Pattern.compile("\\@([0-9a-z\\-]*)\\{(.*?)\\}");
-    private final static Pattern tagPattern = Pattern.compile("\\#([0-9a-z\\-]*)\\{(.*?)\\}");
+    private final static Pattern tagPattern = Pattern.compile("(\\@|\\#)([0-9a-z\\-]*)\\{(.*?)\\}");
 
     public static List<String> extractPersonTags(String text) {
 
         List<String> result = Lists.newArrayList();
 
-        Matcher matcher = namePattern.matcher(text);
+        Matcher matcher = tagPattern.matcher(text);
         while (matcher.find()) {
-            result.add(matcher.group(1));
+            if ("@".equals(matcher.group(1))) {
+                result.add(matcher.group(2));
+            }
         }
         return result;
     }
 
-    public static List<String> extractTags(String text) {
+    public static List<String> extractHashTags(String text) {
 
         List<String> result = Lists.newArrayList();
 
         Matcher matcher = tagPattern.matcher(text);
         while (matcher.find()) {
-            result.add(matcher.group(1));
+            if ("#".equals(matcher.group(1))) {
+                result.add(matcher.group(2));
+            }
         }
         return result;
     }
