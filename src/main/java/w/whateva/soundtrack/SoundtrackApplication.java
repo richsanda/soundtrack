5f93b315ff082fb35dd87fe3e4afcf3c4b6a1edc
rich <richsanda@yahoo.com>
2017-02-20 11:29:09 -0500

    simplify SoundtrackApplication

--- src/main/java/w/whateva/soundtrack/SoundtrackApplication.java
+++ src/main/java/w/whateva/soundtrack/SoundtrackApplication.java
@@ -2,14 +2,12 @@ package w.whateva.soundtrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  *
  */
-
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
@@ -17,9 +15,6 @@ public class SoundtrackApplication {
 
     public static void main(String[] args) throws Exception {
 
-        ApplicationContext context = SpringApplication.run(SoundtrackApplication.class, args);
-
-        //SoundtrackLoadJobRunner runner = context.getBean(SoundtrackLoadJobRunner.class);
-        //runner.run();
+        SpringApplication.run(SoundtrackApplication.class, args);
     }
 }
